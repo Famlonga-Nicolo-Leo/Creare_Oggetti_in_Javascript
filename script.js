45f5ce5545b5f02fcb60882d5f7c28290d22ff81
Famlonga-Nicolo-Leo <famlonga.nicolo@iisgalvanimi.edu.it>
2025-01-29 18:30:46 +0000

    progetto finale: Ho finito la parte principale, ma mi manca ancora la parte grafica.

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const animale1 ={
 
 };
 const animale2 ={
-    specie: "cane",
-    razza: "bassotto",
+    specie: "maiale",
+    razza: "cinta senese",
     zampe:4
 
 };
@@ -19,21 +19,36 @@ const animale3 ={
 
 function visualizza(animale){
     if (animale == 0) {
-        document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
-        "specie"+ animale1.specie + "<br>" +
-        "razza" + animale1.razza + "<br>" +
-        "zampe" + animale1.zampe + "<br>";
+        document.getElementById("visualizza").innerHTML= "caratteristiche del animale: <br>" +
+        "specie: "+ animale1.specie + "<br>" +
+        "razza: " + animale1.razza + "<br>" +
+        "zampe: " + animale1.zampe + "<br>";
     }
     if (animale == 1) {
         document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
-        "specie"+ animale2.specie + "<br>" +
-        "razza" + animale2.razza + "<br>" +
-        "zampe" + animale2.zampe + "<br>";
+        "specie: "+ animale2.specie + "<br>" +
+        "razza: " + animale2.razza + "<br>" +
+        "zampe: " + animale2.zampe + "<br>";
     }
     if (animale == 2) {
         document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
-        "specie"+ animale3["specie"] + "<br>" +
-        "razza" + animale3["razza"] + "<br>" +
-        "zampe" + animale3["zampe"]+ "<br>";
+        "specie: "+ animale3["specie"] + "<br>" +
+        "razza: " + animale3["razza"] + "<br>" +
+        "zampe: " + animale3["zampe"]+ "<br>";
+    }
+}
+
+function riproduci_suono(suono){
+    if (suono == 0) {
+    const audio = document.getElementById("audio_mucca");
+    audio.play();
+    }
+    if (suono == 1) {
+        const audio = document.getElementById("audio_maiale");
+        audio.play();
+        }
+    if (suono == 2) {
+    const audio = document.getElementById("audio_gallina");
+    audio.play();
     }
 }
