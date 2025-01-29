9a8b39dc0b976c577e909c3042b2a4c5109edfc9
Famlonga-Nicolo-Leo <famlonga.nicolo@iisgalvanimi.edu.it>
2025-01-29 10:06:10 +0000

    Ho finito la parte principale, ma mi manca ancora la parte grafica

---
+++ script.js
@@ -0,0 +1,39 @@
+const animale1 ={
+    specie: "mucca",
+    razza: "chianina",
+    zampe:4
+
+};
+const animale2 ={
+    specie: "cane",
+    razza: "bassotto",
+    zampe:4
+
+};
+const animale3 ={
+    specie: "gallina",
+    razza: "andalusiana",
+    zampe:2
+
+};
+
+function visualizza(animale){
+    if (animale == 0) {
+        document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
+        "specie"+ animale1.specie + "<br>" +
+        "razza" + animale1.razza + "<br>" +
+        "zampe" + animale1.zampe + "<br>";
+    }
+    if (animale == 1) {
+        document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
+        "specie"+ animale2.specie + "<br>" +
+        "razza" + animale2.razza + "<br>" +
+        "zampe" + animale2.zampe + "<br>";
+    }
+    if (animale == 2) {
+        document.getElementById("visualizza").innerHTML= "caratteristiche del animale <br>" +
+        "specie"+ animale3["specie"] + "<br>" +
+        "razza" + animale3["razza"] + "<br>" +
+        "zampe" + animale3["zampe"]+ "<br>";
+    }
+}
